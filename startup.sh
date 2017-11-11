e1143de1f7a6258e9e7dfc58b018b7a4ec10fb4d
TJ Horner <me@tjhorner.com>
2017-11-10 21:02:43 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl "https://share.horner.tj/raspi?ip=$(hostname -I | xargs)&hostname=$(hostname | xargs)"
 amixer set PCM 100%
-vlc http://192.168.0.18:8080/
+screen -Sdm musicstream vlc http://192.168.0.18:8080/
