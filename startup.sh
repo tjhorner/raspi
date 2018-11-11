99d5a62d3ab1907fbdb28a3853284a23e71dd4d7
TJ Horner <me@tjhorner.com>
2018-11-10 16:01:45 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 # This script pings the telegram channel with IP and hostname and also connects to the local music stream
 # curl "https://share.horner.tj/raspi?ip=$(hostname -I | xargs)&hostname=$(hostname | xargs)"
 amixer set PCM 100%
-screen -Sdm music cvlc -R http://10.254.57.39:8080/
+screen -Sdm music cvlc -R http://10.254.59.186:8080/
